b44dacf2a2a06af228b80889109b861466137d2f
Kirill Royako <rakot666111@gmail.com>
2023-11-27 11:07:02 +0300

    main: 27 day begin

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="shortcut icon" href="{{ asset('img/pages/home/cthulhuIco2.jpg') }}" type="image/x-icon">
     <!-- Bootstrap CSS -->
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 
     <title>
         @yield('title')
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,12 +6,40 @@
 
 @section('content')
 
-    <div class="container-fluid" id="home">
-        <div class="row">
-            <div class="col-md-12">
-                <h1 class="">Миры Лавкрафта</h1>
-                <img src="/img/pages/home/Main.jpg" alt="CtulhuSea">
+    <div class="backgrond-ctulhu">
+
+    <header>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="mt-5 col-sm-5 offset-sm-2 col-md-8 offset-md-5">
+                    <h1 class="text-center text-uppercase mt-5">Worlds of Lovecraft</h1>
+                </div>
             </div>
         </div>
+    </header>
+
+    <section class="buttons-mid">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl-4 col-md-6 col-sm-12">
+                    <button type="button" class="btn btn-books btn-outline-primary btn-lg">Биография<br>Библиография</button>
+                </div>
+                <div class="col-xl-4 col-md-6 col-sm-12">
+                    <button type="button" class="btn btn-media btn-outline-primary btn-lg">Фильмы<br>Игры</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl-4 col-md-6 col-sm-12">
+                    <button type="button" class="btn btn-creatures btn-outline-primary btn-lg">Бестирарий</button>
+                </div>
+            </div>
+        </div>
+    </footer>
     </div>
+
 @endsection
